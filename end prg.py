36885c82e76389ff5e4aadf2eb147f317016bb9a
siva-cpu <68471333+siva-cpu@users.noreply.github.com>
2020-07-19 11:21:16 +0530

    initial commit

---
+++ end prg.py
@@ -0,0 +1,7 @@
+
+# it will print new line after printing "Hello"
+print("Hello",end="   ")
+print()
+print()
+# it willprint new line after printing "World"
+print("World")
---
+++ function.py
@@ -0,0 +1,8 @@
+def sayhello():
+    print('hello...welcome to python')
+sayhello()
+
+
+def sayhello(name='siva'):
+    print('hello...welcome to python' + str(name))
+sayhello()
